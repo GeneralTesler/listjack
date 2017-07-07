f4fcdcbf99e61876c4dafaf0a02ddf0836ecd7e6
2XXE <GeneralTesler@users.noreply.github.com>
2017-07-07 16:12:59 -0700

    updated setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,8 @@ apt-get install xvfb
 apt-get install libffi-dev
 apt-get install libfuzzy-dev
 apt-get install attr
-wget https://github.com/mozilla/geckodriver/releases/download/v0.17.0/geckodriver-v0.17.0-linux64.tar.gz
+#using this version of geckodriver for compatibility with Kali 
+wget https://github.com/mozilla/geckodriver/releases/download/v0.13.0/geckodriver-v0.13.0-linux64.tar.gz
 tar -xvf geckodriver-v0.17.0-linux64.tar.gz
 rm geckodriver-v0.17.0-linux64.tar.gz
 mv geckodriver /usr/sbin
